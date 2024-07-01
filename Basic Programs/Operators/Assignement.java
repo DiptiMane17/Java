e2b0de8be29319323c06fb0b702be31a8b8c6358
DiptiMane17 <diptimane1703@gmail.com>
2024-07-01 18:53:25 +0530

    Program

---
+++ Basic Programs/Operators/Assignement.java
@@ -0,0 +1,17 @@
+public class Assignement {
+    public static void main(String aargs[] )
+    {
+        int a = 5;
+        int b = 4;
+
+        System.out.println("First Number : " +a);
+        System.out.println("Second Number : "+b);
+
+        System.out.println("a += b :" +(a+=b));
+        System.out.println("a -= b : " +(a-=b));
+        System.out.println("a *= b : " +(a*=b));
+        System.out.println("a /= b : " +(a/=b));
+        System.out.println("a %= b : " +(a%=b));
+    }
+    
+}
---
+++ Basic Programs/Operators/Logical.java
@@ -0,0 +1,17 @@
+public class Logical {
+    public static void main (String args [])
+
+    {
+        boolean a = true;
+        boolean b = false;
+
+        boolean c = a && b;
+        boolean d = a || b;
+
+        boolean e = !a;
+
+        System.out.println("a && b : " +c);
+        System.out.println("a || b : " +d);
+        System.out.println("!a : " +e);
+    }
+}
