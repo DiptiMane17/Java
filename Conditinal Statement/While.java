a8cc2ec8fdd8f5e4b09a3b19e806cfd569ca562b
DiptiMane17 <diptimane1703@gmail.com>
2024-07-03 19:02:34 +0530

    Program

---
+++ Conditinal Statement/While.java
@@ -0,0 +1,12 @@
+public class While {
+    public static void main(String args[])
+    {
+        int a = 0;
+        while(a <5)
+        {
+            System.out.println("a is : " +a);
+            a++;
+        }
+    }
+    
+}
